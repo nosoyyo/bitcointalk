01cc78d26b4c3ca68abce3cce4c0fef37927ecaa
nosoyyo <oyyoson@gmail.com>
2018-04-30 21:22:11 +0800

    init
    simple but functioning

--- config.py
+++ config.py
@@ -9,9 +9,6 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
 
-cookies = {'PHPSESSID': 's4oefuhvef562bia7pp6k7a017', 'SMFCookie129': 'a%3A4%3A%7Bi%3A0%3Bs%3A7%3A%222002475%22%3Bi%3A1%3Bs%3A40%3A%22e4db4c6461db142ea15d4fe43c6343ab6bee9be3%22%3Bi%3A2%3Bi%3A1714284347%3Bi%3A3%3Bi%3A0%3B%7D',
-           '__cfduid': 'def87c90e7a06e1c14dd13f8dda82ff681524625608'}
-
 headers4post = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'accept-encoding': 'gzip, deflate, br',
